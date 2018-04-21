66101dfa8f1657b80a1db43fb40b8791059e4033
unknown <michaszo18@gmail.com>
2018-04-21 13:06:34 +0200

    MikePong v0.1

---
+++ Mike/Assets/Scripts/Ball.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+   
+    [SerializeField]
+    float speed;
+
+    float radius;
+    Vector2 direction;
+    int playerLeft;
+    int playerRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        //direction = Vector2.one.normalized; //direction is (1,1) normalized
+        //radius = transform.localScale.x / 2; //half the width
+
+        rb2d = GetComponent<Rigidbody2D>();
+        GoBall();
+    }
+    
+    
+    
+    //**************************************************
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    void GoBall()
+    {
+        direction = Vector2.one.normalized; //direction is (1,1) normalized 
+        radius = transform.localScale.x / 2; //half the width
+       
+    }
+    void ResetBall()
+    {
+        vel = Vector2.zero;
+        rb2d.velocity = vel;
+        transform.position = Vector2.zero;
+    }
+    void RestartGame()
+    {
+        ResetBall();
+        
+    }
+
+    //**************************************************
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        //Bounce off top and bottom 
+        if (transform.position.y < GameMenager.bottomLeft.y + radius && direction.y < 0)
+        {
+            direction.y = -direction.y;
+        }
+        if (transform.position.y > GameMenager.topRight.y - radius && direction.y > 0)
+        {
+            direction.y = -direction.y;
+        }
+
+        //Game over 
+        if(transform.position.x < GameMenager.bottomLeft.x + radius && direction.x < 0)
+        {
+            playerRight++;
+            Debug.Log(playerRight);
+            NewService();
+
+        }
+        if (transform.position.x > GameMenager.topRight.x - radius && direction.x > 0)
+        {
+            playerLeft++;
+            Debug.Log(playerLeft);
+            NewService();
+        }
+    }
+
+    void NewService()
+    {
+        RestartGame();
+   
+    }
+
+    //flip direction
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Paddle")
+        {
+            bool isRight = other.GetComponent<Paddle>().isRight;
+
+            if (isRight == true && direction.x > 0)
+            {
+                direction.x = -direction.x;
+            }
+            if (isRight == false && direction.x < 0)
+            {
+                direction.x = -direction.x;
+            }
+        }
+    }
+
+}
---
+++ Mike/Assets/Scripts/Paddle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField]
+    public float speed;
+
+    float heightPaddle;
+
+    string input;
+    public bool isRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        heightPaddle = transform.localScale.y;
+        speed = 5f;
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        isRight = isRightPaddle;
+
+        Vector2 pos = Vector2.zero;
+        if (isRightPaddle)
+        {
+            //Place paddle on the right of screen
+            pos = new Vector2(GameMenager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; //move a bit to the left (paddle was outside the sceen)
+
+            input = "PaddleRight";
+        }
+        else
+        {
+            //Place on left
+            pos = new Vector2(GameMenager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;  //move a bit to the right (paddle was outside the sceen)
+
+            input = "PaddleLeft";
+        }
+        //Update this paddle's position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        //Restrict paddle movement
+        //If paddle is to low and user is contiuing to move down, stop
+        if (transform.position.y < GameMenager.bottomLeft.y + heightPaddle / 2 && move < 0)
+        {
+            move = 0;
+        }
+        if (transform.position.y > GameMenager.topRight.y - heightPaddle / 2 && move > 0)
+        {
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up);
+
+    }
+}
